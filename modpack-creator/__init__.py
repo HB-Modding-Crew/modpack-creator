94ba7cc6dbe51da79dbf849c6048550916c003a6
Paul Brancieq <paul.bancieq@epitech.eu>
2023-01-19 12:41:01 +0100

    feat(pip): Basic package

---
+++ modpack-creator/__init__.py
@@ -0,0 +1,10 @@
+"""
+
+modpack-creator
+
+A modpack creator for Minecraft
+"""
+
+__version__ = '1.0.0'
+__author__ = 'Brancieq Paul'
+__doc__ = "A modpack creation and maintenance tool for Minecraft"
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='modpack-creator',
+    description='A tool to create and maintain modpacks for Minecraft',
+    author='Brancieq Paul',
+    license='MIT',
+    packages=['modpack-creator'],
+)
